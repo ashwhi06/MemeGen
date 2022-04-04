72fc2588a0cb930c19c81e98751cabe74cef27e1
ashwhi06 <ashe.white06@gmail.com>
2022-04-04 16:50:06 -0400

    Add Css and classname to header

--- my-meme-generator/src/components/Header.js
+++ my-meme-generator/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import troll from '../images/troll-face.png'
 
 export default function Header() {
     return (
-        <header>
-            <h1>Header Component</h1>
+        <header className="header">
+            <img src={troll} className="header--image" />
+            <h2 className="header--title" >Meme Generator</h2>
+            <h4 className="header--project" >React Course - Project</h4>
         </header>
     )
 }
