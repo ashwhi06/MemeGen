ea6d0601b315a12314999253cc3e0241c132b1b5
ashwhi06 <ashe.white06@gmail.com>
2022-04-05 08:19:40 -0400

    Meme data added

--- my-meme-generator/src/components/Meme.js
+++ my-meme-generator/src/components/Meme.js
@@ -3,12 +3,18 @@ import React from 'react';
 export default function Meme() {
     return(
         <main>
-            <form className="form">
-                <input type="text" placeholder="Top Text here..." className="form--input" />
-                <input type="text" placeholder="Bottom Text here..." className="form--input" />
-                <button className="form--button" >Get a new meme image 🖼</button>
-                <img src="" />
-            </form>
+            <div className="form">
+                <input type="text" 
+                placeholder="Top Text here..." 
+                className="form--input" 
+                />
+                <input type="text" 
+                placeholder="Bottom Text here..." 
+                className="form--input" 
+                />
+                <button 
+                className="form--button" >Get a new meme image 🖼</button>
+            </div>
         </main>
  
     )
